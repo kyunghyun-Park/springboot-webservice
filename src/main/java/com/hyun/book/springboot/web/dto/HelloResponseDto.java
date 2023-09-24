7baf30436c2426d8dfddda51ab0fd09d8c9c14df
kyunghyun-park <qqqq_o@naver.com>
2023-09-24 23:54:24 +0900

    테스트코드 작성 및 롬복전환

---
+++ src/main/java/com/hyun/book/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,13 @@
+package com.hyun.book.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+}
