51848fdc75d129b61d780fe298ef5d6a6b691fc6
kyunghyun-park <qqqq_o@naver.com>
2023-09-26 23:25:45 +0900

    전체 조회 화면 만들기

--- src/main/java/com/hyun/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/hyun/book/springboot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.hyun.book.springboot.service.posts;
 
 import com.hyun.book.springboot.domain.posts.Posts;
 import com.hyun.book.springboot.domain.posts.PostsRepository;
+import com.hyun.book.springboot.web.dto.PostsListResponseDto;
 import com.hyun.book.springboot.web.dto.PostsResponseDto;
 import com.hyun.book.springboot.web.dto.PostsSaveRequestDto;
 import com.hyun.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -37,4 +40,12 @@ public class PostsService {
 
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = false)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
 }
--- src/main/java/com/hyun/book/springboot/web/IndexController.java
+++ src/main/java/com/hyun/book/springboot/web/IndexController.java
@@ -1,15 +1,21 @@
 package com.hyun.book.springboot.web;
 
+import com.hyun.book.springboot.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
+
         return "index";
     }
 
