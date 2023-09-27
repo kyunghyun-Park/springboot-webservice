8791565e8886d8acc7bdcb2c7eebed24b2aa5d9d
kyunghyun-park <qqqq_o@naver.com>
2023-09-28 00:51:11 +0900

    @WebMvcTest는 @Controller빼고 스캔하지 않기 때문에 제거, @Entity가 없어서 나는 에러로 인한 EnableJpaAuditing 위치 수정

--- src/main/java/com/hyun/book/springboot/Application.java
+++ src/main/java/com/hyun/book/springboot/Application.java
@@ -2,9 +2,9 @@ package com.hyun.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing // JPA Auditing 활성화
+//HelloControllerTest.class의 @WebMvcTest는 @Entity클래스가 없어서 제거
+//@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
