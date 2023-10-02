c6fdeeff1cbc24e904cf32a922fd2f7927fb77a1
kyunghyun-park <qqqq_o@naver.com>
2023-10-03 00:28:30 +0900

    배포 자동화를 위한 설정 추가 및 수정

--- src/main/java/com/hyun/book/springboot/Application.java
+++ src/main/java/com/hyun/book/springboot/Application.java
@@ -2,12 +2,15 @@ package com.hyun.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 //HelloControllerTest.class의 @WebMvcTest는 @Entity클래스가 없어서 제거
 //@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication application = new SpringApplication(Application.class);
+        application.addListeners(new ApplicationPidFileWriter());
+        application.run(args);
     }
 }
